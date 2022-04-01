44aa5951e0e723d40dc2ffb161d7971691c973a0
user.name : "00012256 <user.email : "id00012256@gmail.com>
2022-04-01 19:52:01 +0500

    fix bypassing auth with routes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
+// Middlewafre functions to prevent bypassing auth and showing login page after logging in
+function ensureAuth(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect("/");
+  }
+}
+function ensureGuest(req, res, next) {
+  if (req.isAuthenticated()) {
+    res.redirect("/dashboard");
+  } else {
+    return next();
+  }
+}
+
 // Login page
-router.get("/", (req, res) => {
+router.get("/", ensureGuest, (req, res) => {
   res.render("login");
 });
 
 // Dashboard page
-router.get("/dashboard", (req, res) => {
+router.get("/dashboard", ensureAuth, (req, res) => {
   res.render("dashboard");
 });
 
